d3b43d317cb45d36cf65b8ebc36014d3a5a16bcc
jazzminegarg <113499748+jazzminegarg@users.noreply.github.com>
2024-07-14 06:41:29 +0530

    added buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import ToggleMode from './ToggleMode'
 import { TextRevealCard } from './ui/text-reveal-card'
+import MagicButton from './ui/MagicButton'
 
 const Hero = () => {
     return (
@@ -19,7 +20,7 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-teal-300 flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-teal-300 dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
   {/* Radial gradient for the container to give a faded look */}
   <div className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)] dark:[mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 </div>
@@ -28,17 +29,28 @@ const Hero = () => {
     <div className='flex justify-center relative my-20 z-10'>
       <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
 
-        <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+        <h1 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
           A software Engineering Student
-        </h2>
+        </h1>
         <TextRevealCard
         className='text-center text-[40px] md:text-5xl lg:text-6xl'
         text="Hover to know about me"
         revealText='Jasmine Garg'
         >
-
+         <h2 className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Jasmine, a software engineer and an AI/ML enthusiast.
+          </h2>
 
         </TextRevealCard>
+        <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Mission : Change the world through computer science.
+          </p>
+         <a href='#about'>
+          <MagicButton 
+          />
+          </a>
+
+         
       </div>
     </div>
       </div>
